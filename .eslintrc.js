7fb0390c1b5b9f5ce1659e8b9dfb52b81089d4b3
pandanoir <moon_in_france@yahoo.co.jp>
2020-10-10 23:08:15 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+      },
+    ],
+    'eol-last': ['error', 'always'],
+    'react/prop-types': 'off',
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { ESBuildPlugin } = require('esbuild-loader');
+module.exports = {
+  mode: 'development',
+  entry: './src/main.tsx',
+  output: {
+    path: require('path').resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'esbuild-loader',
+          options: {
+            loader: 'tsx',
+            target: 'es2015',
+          },
+        }
+      },
+    ],
+  },
+  plugins: [
+    new ESBuildPlugin()
+  ],
+}
